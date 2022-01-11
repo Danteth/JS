9e341ad0cfbdb12dbb148b39f3d7be5fa8ff893c
Danteth <dserous8h@gmail.com>
2022-01-11 14:56:26 +0100

    5

--- 03MUST/06spread.js
+++ 03MUST/06spread.js
@@ -7,6 +7,25 @@ console.log(languages);
 console.log(dsl);
 console.log(allLanguages);
 
+const courseData = {
+    name: 'Projektownie i programowanie front-end',
+    shortcut: 'PPFront',
+    year: '2',
+    content:{
+        types:{
+            lectures: true,
+            laboratories: true,
+            projects: true
+        },
+        hours:{
+            lectures: 15,
+            laboratories: 15,
+            projects: 15
+        }
+    }
+}
+
+
 const courseDataForYear = {
     start: '2021', 
     end: '2022'
